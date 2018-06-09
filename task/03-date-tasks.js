43b71fe599ae8555e8abc5b6ee28dd92c4d101a1
Zelenschi <zelenschiandrei@gmail.com>
2018-06-09 10:02:54 +0300

    timeSpanToString

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -101,7 +101,9 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   // var pi = Math.PI;
+  //  return date * (pi/180);
+  throw new Error('Not implemented');
 }
 
 
