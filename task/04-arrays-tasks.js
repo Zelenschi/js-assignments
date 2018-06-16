b434bc396d830c618061e2eb95fc36b7ae66494f
Zelenschi <zelenschiandrei@gmail.com>
2018-06-16 15:21:11 +0300

    Returns array containing only unique

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -492,7 +492,7 @@ function getIntervalArray(start, end) {
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
 function distinct(arr) {
-   throw new Error('Not implemented');
+    return arr.filter((el, i, arr) => arr.indexOf(el) == i);
 }
 
 /**
