9d9fd32d938146cd541301ed7478f4669c7b5d02
Zelenschi <zelenschiandrei@gmail.com>
2018-06-23 12:46:21 +0300

    Returns the regexp that matches all IPv4

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -31,7 +31,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-   throw new Error('Not implemented');
+    throw new Error('Not implemented');
 }
 
 
@@ -53,7 +53,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   throw new Error('Not implemented');
+    return /p.t/;
 }
 
 
@@ -72,7 +72,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+    return /^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}).){3}(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})$/;
 }
 
 
