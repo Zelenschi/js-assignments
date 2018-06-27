9283e435c6343c39e86fa6adef40e1f906a57a0e
Zelenschi <zelenschiandrei@gmail.com>
2018-06-27 15:28:20 +0300

    Returns the first non repeated char in the specified strings

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -184,7 +184,14 @@ function isInsideCircle(circle, point) {
  *   'entente' => null
  */
 function findFirstSingleChar(str) {
-    throw new Error('Not implemented');
+  for (var i=0; i<str.length;i++){
+      if (str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i))){
+          return str.charAt(i);
+          break;
+      }
+  } 
+
+
 }
 
 
@@ -244,8 +251,8 @@ function reverseString(str) {
  *   34143 => 34143
  */
 function reverseInteger(num) {
-    throw new Error('Not implemented');
-}
+    throw new Error('Not implemented') }
+
 
 
 /**
